e7503eb02acd7d8479114c9f47a0f0c4c1303a80
Adam Winer <awiner@apache.org>
2006-05-09 06:16:00 +0000

    Repackage javascript plugin from oracle to org.apache.myfaces

---
+++ maven-javascript-plugin/src/main/java/org/apache/myfaces/adfbuild/plugins/javascript/uixtools/Token.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2000-2001,2006 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.myfaces.adfbuild.plugin.javascript.uixtools;
+
+/**
+ * Tokens for JavaScript source code
+ * @version $Name:  $ ($Revision: 1.2 $) $Date: 2004/03/25 02:00:16 $
+ * @author Arjuna Wijeyekoon - arjuna.wijeyekoon@oracle.com
+ */
+public class Token
+{
+
+  public Token(int code, int lineNumber)
+  {
+    this(code, lineNumber, (char) 0, (String) null);
+  }
+
+  public Token(int code, int lineNumber, char ch)
+  {
+    this(code, lineNumber, ch, (String) null);
+  }
+
+  public Token(int code, int lineNumber, String s)
+  {
+    this(code, lineNumber, (char) 0, s);
+  }
+
+  public Token(int code, int lineNumber, char ch, String s)
+  {
+    this.code=code;
+    this.lineNumber = lineNumber;
+    this.ch=ch;
+    string = s;
+  }
+
+  public String toString()
+  {
+    return "Token code:"+code+" line:"+lineNumber+
+      " char:"+ch+" string:"+string;
+  }
+
+  public final int code, lineNumber;
+  public final char ch;
+  public final String string;
+
+  public static final int EOF =                 0;
+  public static final int NEWLINE =             10;
+  public static final int WHITESPACE =          11;
+  public static final int PERIOD =              12;
+  public static final int SEMICOLON =           13;
+  public static final int QUOTED =              20;
+  public static final int NAME =                30;
+  public static final int NUMBER =              40;
+  public static final int COMMENT =             50;
+  public static final int REGULAR_EXP =         60;
+  public static final int REGULAR_EXP_MODIFIER =65;
+  public static final int CONTROL =             100;
+  public static final int LEFT_BRACE =          110;
+  public static final int RIGHT_BRACE =         120;
+  public static final int RESERVED =            200; //reserved words
+
+}
