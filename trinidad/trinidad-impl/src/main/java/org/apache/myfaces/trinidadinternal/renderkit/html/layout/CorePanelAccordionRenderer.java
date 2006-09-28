e94bbaf5b2df88277d9b34a2e75722fa7444202a
Jeanne Waldman <jwaldman@apache.org>
2006-09-28 00:03:35 +0000

    ADFFACES-199 fix panelAccordion component for ppr-disabled browsers like BlackBerry 4.0. Committed for Piyush Hari.

--- trinidad/trinidad-impl/src/main/java/org/apache/myfaces/trinidadinternal/renderkit/html/layout/CorePanelAccordionRenderer.java
+++ trinidad/trinidad-impl/src/main/java/org/apache/myfaces/trinidadinternal/renderkit/html/layout/CorePanelAccordionRenderer.java
@@ -1,12 +1,12 @@
 /*
  * Copyright  2005,2006 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -488,12 +488,17 @@ public class CorePanelAccordionRenderer extends UINodeRendererBase
     }
     else
     {
+      //PH: Previous Code had event set to 'show' (event:'show')
+      //at all times. As a result a showDetail item will 'show'
+      //but won't 'hide'. Therefore, I changed it to event:event.
       StringBuilder onClickHandlerBuff = new StringBuilder("submitForm('")
                                   .append(formName)
                                   .append("',")
                                   .append(validate)
-                                  .append(", {event:'show',source:'")
-                                  .append(detailItemId)
+                                  .append(", {event:'")
+	                           .append(event)
+	                           .append("',source:'")
+	                           .append(detailItemId)
                                   .append("'});return false;");
 
       onClickHandler = onClickHandlerBuff.toString();
