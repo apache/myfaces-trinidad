d67bdd8d32658cd96fce182f98f8871830320517
Adam Winer <awiner@apache.org>
2006-10-19 21:05:26 +0000

    Rewrite translations resource loader to be much more performant, properly support non-ASCII characters, more generic, comply with coding standards, easily subclassed, etc.

--- trinidad-impl/src/main/java/org/apache/myfaces/trinidadinternal/resource/TrTranslationsResourceLoader.java
+++ trinidad-impl/src/main/java/org/apache/myfaces/trinidadinternal/resource/TrTranslationsResourceLoader.java
@@ -15,17 +15,11 @@
 */
 package org.apache.myfaces.trinidadinternal.resource;
 
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.net.URL;
+import javax.faces.context.FacesContext;
 
-import org.apache.myfaces.trinidad.resource.StringContentResourceLoader;
-import org.apache.myfaces.trinidad.util.ClassLoaderUtils;
+import org.apache.myfaces.trinidad.skin.Skin;
 
-import org.apache.myfaces.trinidadinternal.renderkit.core.xhtml.XhtmlUtils;
-
-public class TrTranslationsResourceLoader extends StringContentResourceLoader
+public class TrTranslationsResourceLoader extends TranslationsResourceLoader
 {
   /**
    * Constructs a dynamic resouce loader for this path which serves up translations
@@ -35,116 +29,37 @@ public class TrTranslationsResourceLoader extends StringContentResourceLoader
   public TrTranslationsResourceLoader(String path)
   {
     super(path);
-    setMap("TrMessageFactory._TRANSLATIONS");
-    setBundle("org.apache.myfaces.trinidad.resource.MessageBundle");
   }
-  
-  protected void setMap(String mapName)
-  {
-    this.mapName = mapName;
-  }
-  
-  protected void setBundle(String bundleName)
-  {
-    this.bundleName = bundleName;
-  }
-  
+
+
   @Override
-  protected String getString(String path) throws IOException
+  protected int getDefaultSize()
   {
-    // its always better to initialize the StringBuffer with size instead of leaving it unset
-    StringBuffer bundleMap = new StringBuffer(50000);
-    String content   = "";
-    
-    String locale = CoreRenderKitResourceLoader.getLocale();
-    
-    bundleMap.append(mapName)
-             .append(" = ")
-             .append("\n{\n");
-    
-    content += _processBundle(bundleName, locale);
-    
-    //Remove the last 2 characters( ie, newLine & ',')
-    bundleMap.append(content.substring(0, content.length()-2))
-             .append("\n};");
-    
-    return bundleMap.toString();
+    // We're coming in at about 13K for Japanese, which is probably
+    // about as big as this will get
+    return 20000;
   }
   
+  protected String getJSVarName()
+  {
+    return "TrMessageFactory._TRANSLATIONS";
+  }
+
+  protected String getBundleName()
+  {
+    return "org.apache.myfaces.trinidad.resource.MessageBundle";
+  }
+
   @Override
-  protected String getContentType(String path)
+  protected String getLocaleString(FacesContext context)
   {
-    return _CONTENT_TYPE;
+    return CoreRenderKitResourceLoader.getLocale();
   }
-  
+
+  // These translations do not go through the skin
   @Override
-  protected URL findResource(
-    String path) throws IOException
+  protected Skin getSkin(FacesContext context)
   {
-    return getURL(path);
+    return null;
   }
-  
-  protected String _processBundle(
-    String  bundleName,
-    String  localeName
-    ) throws IOException
-  {
-    StringBuffer transMap = new StringBuffer(50000);
-    Object obj = invokeGetContents(bundleName, localeName);
-    
-    if( obj != null)
-    {
-      Object entry[] = (Object [])obj;
-      
-      for(int i=0; i< entry.length; i++)
-      {
-        transMap.append("'" + ((Object [])entry[i])[0] + "'")
-                .append(":")
-                .append("'" + XhtmlUtils.escapeJS(((Object [])entry[i])[1].toString(), true) + "',")
-                .append("\n");
-      }
-    }
-    
-    return transMap.toString();
-  }
-  
-  protected Object invokeGetContents(
-    String  bundleName,
-    String  localeName
-    ) throws IOException
-  {
-    String className  = bundleName + "_" + localeName; 
-    Object obj = null;
-    
-    try
-    {
-      Class<?> clazz;
-      try
-      {
-        clazz = ClassLoaderUtils.loadClass(className);
-      }
-      catch (ClassNotFoundException e)    
-      { 
-        // If there is no bundle specific to locale, use Default locale
-        clazz = ClassLoaderUtils.loadClass(bundleName);    
-      }
-      
-      //Invoke getContents() method which gives us the Translations
-      Method x = clazz.getMethod("getContents", (Class[]) null);  
-      obj = x.invoke(clazz.newInstance(), (Object[]) null);
-    
-    } catch (NoSuchMethodException e)     { ;
-    } catch (InvocationTargetException e) { ;
-    } catch (IllegalAccessException e)    { ;
-    } catch (InstantiationException e)    { ;
-    } catch (ClassNotFoundException e)    { ;
-    }
-    
-    return obj;  
-  }
-  
-  protected String mapName;
-  protected String bundleName;
-  
-  private static final String _CONTENT_TYPE = "text/javascript";
 }
