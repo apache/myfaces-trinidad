0412afc96e5196c145707c8be2414bc7ff206f30
Matthias Wessendorf <matzew@apache.org>
2007-01-16 14:15:53 +0000

    merged overhaul from plugins release brunch to trunk: -The overhaul was mainly adding the proper (correct) ASF license header to all files, which are inside of the src/ folder. -Also added the ASF license header to the Maven-Faces-Plugin to ensure that all generated components contain the same proper header as well -Finally deleted two empty oracle folders, because not needed.

--- maven-faces-plugin/src/main/java/org/apache/myfaces/trinidadbuild/plugin/faces/AbstractFacesMojo.java
+++ maven-faces-plugin/src/main/java/org/apache/myfaces/trinidadbuild/plugin/faces/AbstractFacesMojo.java
@@ -1,17 +1,20 @@
 /*
-* Copyright 2006 The Apache Software Foundation.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
+*            Licensed to the Apache Software Foundation (ASF) under one
+*            or more contributor license agreements.  See the NOTICE file
+*            distributed with this work for additional information
+*            regarding copyright ownership.  The ASF licenses this file
+*            to you under the Apache License, Version 2.0 (the
+*            "License"); you may not use this file except in compliance
+*            with the License.  You may obtain a copy of the License at
+* 
+*              http://www.apache.org/licenses/LICENSE-2.0
+* 
+*            Unless required by applicable law or agreed to in writing,
+*            software distributed under the License is distributed on an
+*            "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+*            KIND, either express or implied.  See the License for the
+*            specific language governing permissions and limitations
+*            under the License.
 */
 package org.apache.myfaces.trinidadbuild.plugin.faces;
 
@@ -600,7 +603,22 @@ abstract public class AbstractFacesMojo extends AbstractMojo
 "//          or you will lose your changes.\n\n";
 
   static private final String _COPYRIGHT =
-"/*\n"                                                                         +
-"** (TBD - insert proper license in generated code).\n"             +
-"*/\n";
+                  "/*\n" +
+                  " * Licensed to the Apache Software Foundation (ASF) under one\n" +
+                  " * or more contributor license agreements.  See the NOTICE file\n" +
+                  " * distributed with this work for additional information\n" +
+                  " * regarding copyright ownership.  The ASF licenses this file\n" +
+                  " * to you under the Apache License, Version 2.0 (the\n" +
+                  " * \"License\"); you may not use this file except in compliance\n" +
+                  " * with the License.  You may obtain a copy of the License at\n" +
+                  " *\n" +
+                  " *   http://www.apache.org/licenses/LICENSE-2.0\n" +
+                  " *\n" +
+                  " * Unless required by applicable law or agreed to in writing,\n" +
+                  " * software distributed under the License is distributed on an\n" +
+                  " * \"AS IS\" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY\n" +
+                  " * KIND, either express or implied.  See the License for the\n" +
+                  " * specific language governing permissions and limitations\n" +
+                  " * under the License.\n" +
+                  "*/\n";
 }
