ee632dec5149233a5af95155cdfd318a146ae249
Jeanne Waldman <jwaldman@apache.org>
2007-09-14 18:44:34 +0000

    https://issues.apache.org/jira/browse/TRINIDAD-703 Make image loading more secure The code change is in ClassLoaderResourceLoader.java. LoggerBundle.xrts has 2 new translation strings The other classes are comment additions

--- trinidad-api/src/main/java/org/apache/myfaces/trinidad/resource/ClassLoaderResourceLoader.java
+++ trinidad-api/src/main/java/org/apache/myfaces/trinidad/resource/ClassLoaderResourceLoader.java
@@ -22,10 +22,14 @@ import java.io.IOException;
 
 import java.net.URL;
 
+import org.apache.myfaces.trinidad.logging.TrinidadLogger;
+
 
 /**
  * A resource loader implementation which loads resources
- * using the context class loader.
+ * using the context class loader.  The returned resource URL will be null
+ * for paths that attempt to access paths outside the root directory by having
+ * ".." in the path.
  *
  */
 public class ClassLoaderResourceLoader extends ResourceLoader
@@ -54,6 +58,7 @@ public class ClassLoaderResourceLoader extends ResourceLoader
    * level resource package.
    *
    * @param rootPackage  the top level package used to interpret resource paths
+   * For example, it could be "META-INF".
    */
   public ClassLoaderResourceLoader(
     String rootPackage)
@@ -95,6 +100,12 @@ public class ClassLoaderResourceLoader extends ResourceLoader
         path = path.substring(1);
     }
 
+    if (!_isPathWithinRoot(path))
+    {
+      _LOG.severe("RESOURCE_PATH_OUTSIDE_ROOT", path);
+      return null;
+    }
+    
     return getClassLoader().getResource(path);
   }
 
@@ -128,6 +139,65 @@ public class ClassLoaderResourceLoader extends ResourceLoader
 
     return rootPackage.replace('.', '/');
   }
+  
+  /**
+   * Checks to see if the path when ".." are resolved out is within the root.
+   * Returns true if the path is within the root, otherwise returns false.
+   * e.g., /afr/../../foo.gif is out of root. It gets resolved to /../foo.gif.
+   * /afr/../tmp/../xyz/foo.gif is within the root. It gets resolved to
+   * /xyz/foo.gif
+   * 
+   * @param path -String the path
+   * @return boolean true if the path does not get resolved outside the root
+   */
+  private static boolean _isPathWithinRoot(String path)
+  {
+    if (path.indexOf("..") == -1)
+      return true;
+      
+    // It would be strange if the path has ".." in it because
+    // the browsers (IE7 and Firefox 1.5) resolve ".." out of the path before
+    // requesting the resource.
+    // Therefore we warn if we find a path with "..".
+    // Then, we try to resolve it ourselves to find out if it is outside
+    // the root.
+    _LOG.warning("RESOURCE_PATH_DOTS", path);
+    
+    while (path != null && path.length() > 0 )
+    {
+      // as soon as it starts with .. or /.., we know we are out of bounds
+      if (path.startsWith(".."))
+        return false;
+      int index = path.indexOf("/../");
+      if (index == 0)
+        return false;
+    
+      // couldn't find "/../" in the path, so we are within the root
+      if (index == -1)
+        return true;
+    
+      // resolve out the first match of "/../" by taking it out as well
+      // as the /foo/ before it. 
+      String beginning = path.substring(0, index);
+      String end = path.substring(index+4);
+
+      int lastIndex = beginning.lastIndexOf("/");
+      if (lastIndex == -1)
+      {
+        //could not find / in the beginning string, so strip the entire thing
+        path = end;
+      }
+      else
+      {
+        path = beginning.substring(0, lastIndex+1) +  end;
+      }
+    } 
+    
+    return true;
+  }
+  
 
   private final String _resourcePrefix;
+  private static final TrinidadLogger _LOG = TrinidadLogger.createTrinidadLogger(
+    ClassLoaderResourceLoader.class);
 }
