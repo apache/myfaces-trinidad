bd33b43938956b985c337ed6aa4133fc17e9a0db
Matthias Wessendorf <matzew@apache.org>
2008-01-09 22:10:26 +0000

    TRINIDAD-854

--- trinidad-api/src/main/java/org/apache/myfaces/trinidad/bean/FacesBeanImpl.java
+++ trinidad-api/src/main/java/org/apache/myfaces/trinidad/bean/FacesBeanImpl.java
@@ -52,8 +52,6 @@ abstract public class FacesBeanImpl implements FacesBean
 
   final public Object getProperty(PropertyKey key)
   {
-    _checkNotListKey(key);
-
     Object o = getLocalProperty(key);
     if (o != null)
       return o;
@@ -79,8 +77,6 @@ abstract public class FacesBeanImpl implements FacesBean
    */
   final public Object getRawProperty(PropertyKey key)
   {
-    _checkNotListKey(key);
-
     Object local = getLocalProperty(key);
     if (local != null)
       return local;
