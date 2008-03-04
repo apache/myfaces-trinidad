b092995a87a85bb1ce7f7ee71a37a07b23a746da
Matthew Cooper <mcooper@apache.org>
2008-03-04 17:34:38 +0000

    TRINIDAD-986 Thanks to Gary van Matre for his patch.

--- trinidad-impl/src/main/javascript/META-INF/adf/jsLibs/Window.js
+++ trinidad-impl/src/main/javascript/META-INF/adf/jsLibs/Window.js
@@ -589,6 +589,15 @@ function _sizeWin(
                      : _getBodyWidth(body, body.offsetWidth, body.offsetLeft);
     var newHeight = 0;
 
+    // if the height was not explicitly set, change to auto forcing
+    // recalculation of  the offsetHeight.  FireFox doesn't always detect
+    // that a PPR has changed the content size.
+    var bodyStyle = body.style;
+    if (!bodyStyle.height || bodyStyle.height.length == 0)
+    {
+      bodyStyle.height = "auto";
+    }
+
     if (isStandard)
     {
       newHeight = body.offsetHeight + (window.outerHeight - window.innerHeight);
