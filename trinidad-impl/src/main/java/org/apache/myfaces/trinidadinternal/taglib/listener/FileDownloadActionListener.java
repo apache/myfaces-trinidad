786177559a0614d07d2348fd6bb245fdb1e448e1
Gabrielle Crawford <gcrawford@apache.org>
2008-03-21 18:40:38 +0000

    TRINIDAD-1023 filedownloadActionListener does not tell user when there are errors
    Catch exceptions, reset the response, add an error message to the faces context and call renderResponse.

--- trinidad-impl/src/main/java/org/apache/myfaces/trinidadinternal/taglib/listener/FileDownloadActionListener.java
+++ trinidad-impl/src/main/java/org/apache/myfaces/trinidadinternal/taglib/listener/FileDownloadActionListener.java
@@ -19,12 +19,11 @@
 package org.apache.myfaces.trinidadinternal.taglib.listener;
 
 import java.io.BufferedOutputStream;
-import java.io.IOException;
 import java.io.OutputStream;
 
 import javax.el.MethodExpression;
-import javax.el.ValueExpression;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.component.StateHolder;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
@@ -37,6 +36,8 @@ import org.apache.myfaces.trinidad.bean.FacesBeanImpl;
 import org.apache.myfaces.trinidad.bean.PropertyKey;
 import org.apache.myfaces.trinidad.logging.TrinidadLogger;
 import org.apache.myfaces.trinidad.util.ComponentUtils;
+import org.apache.myfaces.trinidad.util.MessageFactory;
+
 
 /**
  * @todo Look at moving to org.apache.myfaces.trinidad.event
@@ -58,6 +59,14 @@ public class FileDownloadActionListener extends FacesBeanImpl
     TYPE.registerKey("method",
                      MethodExpression.class,
                      PropertyKey.CAP_NOT_BOUND);
+  
+
+  /**
+    * <p>The message identifier of the {@link FacesMessage} to be created when
+    * there is a download error.</p>
+    */
+     public static final String DOWNLOAD_MESSAGE_ID =
+         "org.apache.myfaces.trinidad.event.FileDownloadActionListener.DOWNLOAD_ERROR";  
 
   static
   {
@@ -81,9 +90,9 @@ public class FileDownloadActionListener extends FacesBeanImpl
     }
     else
     {
+      HttpServletResponse hsr = (HttpServletResponse) response;
       try
       {
-        HttpServletResponse hsr = (HttpServletResponse) response;
         if (contentType != null)
           // TODO: encoding?
           hsr.setContentType(contentType);
@@ -91,21 +100,21 @@ public class FileDownloadActionListener extends FacesBeanImpl
           // TODO: what about non-ASCII characters in the filename?
           hsr.setHeader("Content-Disposition",
                         "attachment; filename=" + filename);
+        
         MethodExpression method = getMethod();
         OutputStream out = new BufferedOutputStream(hsr.getOutputStream());
-        try
-        {
-          method.invoke(context.getELContext(), new Object[]{context, out});
-        }
-        finally
-        {
-          out.close();
-        }
+        method.invoke(context.getELContext(), new Object[]{context, out});
+        out.close();
          
       }
-      catch (IOException ioe)
+      catch (Exception e)
       {
-        _LOG.warning(ioe);
+        hsr.reset();
+        _LOG.warning(e);       
+        FacesMessage message = MessageFactory.getMessage(context, DOWNLOAD_MESSAGE_ID);
+        context.addMessage(null, message);
+        context.renderResponse();
+        return;
       }
     }
     
