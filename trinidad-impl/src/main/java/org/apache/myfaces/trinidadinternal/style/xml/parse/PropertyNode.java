dedae3466180530ded1f4029ee1f84168f94bc23
Jeanne Waldman <jwaldman@apache.org>
2008-05-02 23:46:04 +0000

    TRINIDAD-1063 improve performance in PropertyNode intern() name to keep the memory consumption down. trunk_1.2.x

--- trinidad-impl/src/main/java/org/apache/myfaces/trinidadinternal/style/xml/parse/PropertyNode.java
+++ trinidad-impl/src/main/java/org/apache/myfaces/trinidadinternal/style/xml/parse/PropertyNode.java
@@ -37,7 +37,11 @@ public class PropertyNode
     if (name == null || "".equals(name))
       throw new IllegalArgumentException(_LOG.getMessage(
         "PROPERTYNODE_NAME_CANNOT_BE_NULL_OR_EMPTY", new Object[]{name, value}));
-    _name = name;
+
+    // intern() name because many of the property names are the same,
+    // like color, background-color, background-image, font-size, etc.
+    // This will improve the memory used.
+    _name = name.intern();
     _value = value;
   }
 
@@ -56,7 +60,7 @@ public class PropertyNode
   {
     return _value;
   }
-  
+
   @Override
   public boolean equals(Object obj)
   {
@@ -85,11 +89,11 @@ public class PropertyNode
   @Override
   public String toString()
   {
-    return 
+    return
       "[name="   + _name   + ", " +
       "value=" + _value + "]";
   }
-  
+
   private final String _name;
   private final String _value;
   private static final TrinidadLogger _LOG = TrinidadLogger.createTrinidadLogger(
