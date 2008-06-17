09517b40cd9f617aad9bf07d78c84f613a7ced6b
Matthias Wessendorf <matzew@apache.org>
2008-06-17 05:03:06 +0000

    TRINIDAD-1125 - IE7: textarea with maxlength is always one char to long

--- trinidad-impl/src/main/java/org/apache/myfaces/trinidadinternal/renderkit/core/xhtml/SimpleInputTextRenderer.java
+++ trinidad-impl/src/main/java/org/apache/myfaces/trinidadinternal/renderkit/core/xhtml/SimpleInputTextRenderer.java
@@ -521,22 +521,22 @@ public class SimpleInputTextRenderer extends FormInputRenderer
   }
 
   @Override
-  protected String getOnkeypress(
+  protected String getOnkeyup(
     FacesBean bean
     )
   {
-    String onKeyPress = super.getOnkeypress(bean);
+    String onKeyup = super.getOnkeyup(bean);
     if (isTextArea(bean))
     {
       Number maxLength = getMaximumLength(bean);
       if (maxLength != null)
       {
-        onKeyPress = _getMaxLengthFunction(onKeyPress,
-                                           maxLength.intValue());
+        onKeyup = _getMaxLengthFunction(onKeyup,
+                                        maxLength.intValue());
       }
     }
 
-    return onKeyPress;
+    return onKeyup;
   }
 
   @Override
