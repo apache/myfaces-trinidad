15a531372df429e911ceebc4b253bcb772698b42
Matthew Cooper <mcooper@apache.org>
2008-06-19 21:28:28 +0000

    TRINIDAD-1128 - Safari support in FileDownloadActionListener
    Thanks Kamran for his patch.
    FileDownLoadActionListener needs to use UTF-8 encoding for file names in Safari.

--- trinidad-impl/src/main/java/org/apache/myfaces/trinidadinternal/taglib/listener/FileDownloadActionListener.java
+++ trinidad-impl/src/main/java/org/apache/myfaces/trinidadinternal/taglib/listener/FileDownloadActionListener.java
@@ -101,13 +101,16 @@ public class FileDownloadActionListener extends FacesBeanImpl
           hsr.setContentType(contentType);
         if (filename != null)
         {
-          boolean isIE = false;
+          // check for supported user agents. Currently IE, Gecko, and WebKit.
+          // IE and WebKit use UTF-8 encoding.
+          boolean isGecko = true;
           Map<String, String> headers = context.getExternalContext().getRequestHeaderMap();
-          if (headers.get("User-Agent").contains("MSIE"))
-            isIE = true;
+          String agentName = headers.get("User-Agent").toLowerCase();
+          if (agentName.contains("msie") || agentName.contains("applewebkit") || agentName.contains("safari"))
+            isGecko = false;
           // boolean isIE = CoreRenderer.isIE(RenderingContext.getCurrentInstance());
           hsr.setHeader("Content-Disposition",
-                        "attachment; filename=" + MimeUtility.encodeHTTPHeader(filename, isIE));
+                        "attachment; filename=" + MimeUtility.encodeHTTPHeader(filename, !isGecko));
         }
         MethodExpression method = getMethod();
         OutputStream out = new BufferedOutputStream(hsr.getOutputStream());
