f268be02a39cb008af20b05816999de6f5c69032
Matthias Wessendorf <matzew@apache.org>
2008-06-24 03:09:23 +0000

    TRINIDAD-1129 - Server-side validation does not work when using Sun JSF implementation

--- trinidad-api/src/main/java/org/apache/myfaces/trinidad/validator/DoubleRangeValidator.java
+++ trinidad-api/src/main/java/org/apache/myfaces/trinidad/validator/DoubleRangeValidator.java
@@ -366,6 +366,9 @@ public class DoubleRangeValidator extends javax.faces.validator.DoubleRangeValid
   public void restoreState(FacesContext context, Object state)
   {
     _facesBean.restoreState(context, state);
+    // Done for TRINIDAD-1129
+    super.setMaximum(getMaximum());
+    super.setMinimum(getMinimum());
   }
 
   /**
