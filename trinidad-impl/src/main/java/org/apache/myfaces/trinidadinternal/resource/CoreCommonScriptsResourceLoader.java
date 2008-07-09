2d425a76052be68dd5a54aa49976b65e9bf52d8c
Matthias Wessendorf <matzew@apache.org>
2008-07-09 08:14:29 +0000

    TRINIDAD-1149 - ApacheChart.js resource loading problems
    Thanks to Andy Schwartz for this patch.
    We now get a nice PERF. improvement on pages that don't contain the chart!

--- trinidad-impl/src/main/java/org/apache/myfaces/trinidadinternal/resource/CoreCommonScriptsResourceLoader.java
+++ trinidad-impl/src/main/java/org/apache/myfaces/trinidadinternal/resource/CoreCommonScriptsResourceLoader.java
@@ -74,7 +74,6 @@ public class CoreCommonScriptsResourceLoader extends AggregatingResourceLoader
     "META-INF/adf/jsLibs/Shuttle.js",
     "META-INF/adf/jsLibs/PanelPopup.js",
     "META-INF/adf/jsLibs/PopupDialog.js",
-    "META-INF/adf/jsLibs/ApacheChart.js",
     "META-INF/adf/jsLibs/Page.js",
     "META-INF/adf/jsLibs/StatusIndicator.js",
     // XMMLHttp libraries
@@ -108,7 +107,6 @@ public class CoreCommonScriptsResourceLoader extends AggregatingResourceLoader
     "META-INF/adf/jsLibsDebug/Shuttle.js",
     "META-INF/adf/jsLibsDebug/PanelPopup.js",
     "META-INF/adf/jsLibsDebug/PopupDialog.js",
-    "META-INF/adf/jsLibsDebug/ApacheChart.js",
 
     "META-INF/adf/jsLibsDebug/Page.js",
     "META-INF/adf/jsLibsDebug/StatusIndicator.js",
