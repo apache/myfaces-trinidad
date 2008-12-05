0c44d3d039ed688d05d92c1710b0c98d5902e343
Matthias Wessendorf <matzew@apache.org>
2008-12-05 08:33:50 +0000

    TRINIDAD-1331 - BlackBerry 8300 shows NOSCRIPT message when JavaScript is disabled TRINIDAD-1332 - BlackBerry Bold and Storm do not work
    Thanks to Tadashi Enomori for these patches

--- trinidad-impl/src/main/java/org/apache/myfaces/trinidadinternal/renderkit/core/xhtml/BodyRenderer.java
+++ trinidad-impl/src/main/java/org/apache/myfaces/trinidadinternal/renderkit/core/xhtml/BodyRenderer.java
@@ -126,13 +126,10 @@ public class BodyRenderer extends PanelPartialRootRenderer
     // Since we are supporting Non-JavaScript browsers of generic PDA, 
     // we don't need to prompt any message in the client side regarding 
     // the JavaScript capability of generic PDA browsers.
-    if (!isGenericPDA(arc))
-    {
-      _renderNoScript(context, arc);
-    }
 
     if (supportsScripting(arc))
     {
+      _renderNoScript(context, arc);
       _storeInitialFocus(arc, bean);
     }
 
