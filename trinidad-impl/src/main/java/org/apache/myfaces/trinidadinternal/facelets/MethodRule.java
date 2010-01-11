8f9ac33d6c7d2a3487f6d74b1881f4f784098a15
Max Starets <mstarets@apache.org>
2010-01-11 15:22:03 +0000

    TRINIDAD-1615 - Need MetaTagHandler implementation for the Facelets package

--- trinidad-impl/src/main/java/org/apache/myfaces/trinidadinternal/facelets/MethodRule.java
+++ trinidad-impl/src/main/java/org/apache/myfaces/trinidadinternal/facelets/MethodRule.java
@@ -18,118 +18,15 @@
  */
 package org.apache.myfaces.trinidadinternal.facelets;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-
-import javax.el.MethodExpression;
-import javax.faces.el.MethodBinding;
-
-import javax.faces.view.facelets.FaceletContext;
-import javax.faces.view.facelets.MetaRule;
-import javax.faces.view.facelets.Metadata;
-import javax.faces.view.facelets.MetadataTarget;
-import javax.faces.view.facelets.TagAttribute;
-import javax.faces.view.facelets.TagAttributeException;
 
 /**
- * Implementation copied from Facelets 1.1.14, as it got hidden by JSF 2.0
+ * This class has been moved to the API jar. 
+ * Keeping implementation here for compatibility reasons. 
  */
-public final class MethodRule extends MetaRule {
-
-    private final String methodName;
-
-    private final Class returnTypeClass;
-
-    private final Class[] params;
+public final class MethodRule extends org.apache.myfaces.trinidad.facelets.MethodRule {
 
     public MethodRule(String methodName, Class returnTypeClass, Class[] params) {
-        this.methodName = methodName;
-        this.returnTypeClass = returnTypeClass;
-        this.params = params;
+        super(methodName, returnTypeClass, params);
     }
-
-    public Metadata applyRule(String name, TagAttribute attribute,
-            MetadataTarget meta) {
-        if (false == name.equals(this.methodName))
-            return null;
-
-        if (MethodBinding.class.equals(meta.getPropertyType(name))) {
-            Method method = meta.getWriteMethod(name);
-            if (method != null) {
-                return new MethodBindingMetadata(method, attribute,
-                        this.returnTypeClass, this.params);
-            }
-        } else if (MethodExpression.class.equals(meta.getPropertyType(name))) {
-            Method method = meta.getWriteMethod(name);
-            if (method != null) {
-                return new MethodExpressionMetadata(method, attribute,
-                        this.returnTypeClass, this.params);
-            }
-        }
-
-        return null;
-    }
-
-    private class MethodBindingMetadata extends Metadata {
-        private final Method _method;
-
-        private final TagAttribute _attribute;
-
-        private Class[] _paramList;
-
-        private Class _returnType;
-
-        public MethodBindingMetadata(Method method, TagAttribute attribute,
-                Class returnType, Class[] paramList) {
-            _method = method;
-            _attribute = attribute;
-            _paramList = paramList;
-            _returnType = returnType;
-        }
-
-        public void applyMetadata(FaceletContext ctx, Object instance) {
-            MethodExpression expr = _attribute.getMethodExpression(ctx,
-                    _returnType, _paramList);
-
-            try {
-                _method.invoke(instance,
-                        new Object[] { new LegacyMethodBinding(expr) });
-            } catch (InvocationTargetException e) {
-                throw new TagAttributeException(_attribute, e.getCause());
-            } catch (Exception e) {
-                throw new TagAttributeException(_attribute, e);
-            }
-        }
-    }
-
-    private class MethodExpressionMetadata extends Metadata {
-        private final Method _method;
-
-        private final TagAttribute _attribute;
-
-        private Class[] _paramList;
-
-        private Class _returnType;
-
-        public MethodExpressionMetadata(Method method, TagAttribute attribute,
-                Class returnType, Class[] paramList) {
-            _method = method;
-            _attribute = attribute;
-            _paramList = paramList;
-            _returnType = returnType;
-        }
-
-        public void applyMetadata(FaceletContext ctx, Object instance) {
-            MethodExpression expr = _attribute.getMethodExpression(ctx,
-                    _returnType, _paramList);
-
-            try {
-                _method.invoke(instance, new Object[] { expr });
-            } catch (InvocationTargetException e) {
-                throw new TagAttributeException(_attribute, e.getCause());
-            } catch (Exception e) {
-                throw new TagAttributeException(_attribute, e);
-            }
-        }
-    }
+   
 }
