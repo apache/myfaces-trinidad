e0208d7a51148a31ac3aa50755b93ffc635fa611
Gabrielle Crawford <gcrawford@apache.org>
2010-07-12 22:39:48 +0000

    TRINIDAD-1849 subform in table not working

---
+++ trinidad-examples/trinidad-demo/src/main/java/org/apache/myfaces/trinidaddemo/SubformBean.java
@@ -0,0 +1,76 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ * 
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package org.apache.myfaces.trinidaddemo;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.faces.event.ActionEvent;
+
+
+public class SubformBean 
+{
+
+  public SubformBean()
+  {
+ 
+  }
+  public List<MyItem> getTexts2()
+  {
+    return sItems2;
+  }
+  
+  public List<MyItem> getTexts()
+  {
+    return sItems;
+  }
+
+  private static final List<MyItem> sItems = Arrays.asList(new MyItem(null),
+                                                           new MyItem(null), new MyItem(null));
+
+  private static final List<MyItem> sItems2 = Arrays.asList(new MyItem(null),
+                                                           new MyItem(null), new MyItem(null));
+
+
+  public static class MyItem
+  {
+    public MyItem(String text)
+    {
+      mText = text;
+    }
+    
+    public String getText()
+    {
+      return mText;
+    }
+    
+    public void setText(String text)
+    {
+      System.out.println("setter called with " + text);
+      mText = text;
+    }
+    
+    public void doSomething(ActionEvent event)
+    {
+      System.out.println("in doSomething(), value is" + mText);
+    }
+    
+    private String mText;
+  }
+}
