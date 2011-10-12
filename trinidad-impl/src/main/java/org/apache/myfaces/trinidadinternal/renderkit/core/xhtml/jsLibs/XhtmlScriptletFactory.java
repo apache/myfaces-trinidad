b242c0aff1843dd9a2d3c7005c79b7eec43b50e1
Max Starets <mstarets@apache.org>
2011-10-12 17:04:42 +0000

    TRINIDAD-2100 - LocaleInfoScriptlet was missing in the CharSets dependency list

--- trinidad-impl/src/main/java/org/apache/myfaces/trinidadinternal/renderkit/core/xhtml/jsLibs/XhtmlScriptletFactory.java
+++ trinidad-impl/src/main/java/org/apache/myfaces/trinidadinternal/renderkit/core/xhtml/jsLibs/XhtmlScriptletFactory.java
@@ -188,6 +188,7 @@ public class XhtmlScriptletFactory
                            new String[]
                            {
                              "TrValidator()",
+                             LocaleInfoScriptlet.LOCALE_INFO_KEY
                            });
 
     _sShuttleScriptlet =
