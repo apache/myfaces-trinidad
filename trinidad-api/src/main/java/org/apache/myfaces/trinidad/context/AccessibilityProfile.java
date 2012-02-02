a69275bd6a6f7782db9715c8a4d4c7401c4062b9
Gabrielle Crawford <gcrawford@apache.org>
2012-02-02 22:22:27 +0000

    TRINIDAD-2155 Incomplete serialization implementation in AccessibilityProfile$SerializableAccessibilityProfile
    Thanks to Prakash

--- trinidad-api/src/main/java/org/apache/myfaces/trinidad/context/AccessibilityProfile.java
+++ trinidad-api/src/main/java/org/apache/myfaces/trinidad/context/AccessibilityProfile.java
@@ -166,7 +166,7 @@ public class AccessibilityProfile
 
   //Serialization for SerializableAccessibilityProfile internal subclass requires no-arg constructor
   //access of at least package security level.
-  protected AccessibilityProfile()
+  AccessibilityProfile()
   {
     this(ColorContrast.STANDARD, FontSize.MEDIUM);
   }
