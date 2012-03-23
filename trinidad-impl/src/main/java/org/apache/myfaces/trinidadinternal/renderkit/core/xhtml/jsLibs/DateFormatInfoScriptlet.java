80f8d4bf672dfb2c650a053dc94c3a66d0a097d6
Gabrielle Crawford <gcrawford@apache.org>
2012-03-23 18:05:35 +0000

    TRINIDAD-2213 DateFormatInfoScriptlet not getting 2 digit year start from same place as DateTimeConverter, using different values.

--- trinidad-impl/src/main/java/org/apache/myfaces/trinidadinternal/renderkit/core/xhtml/jsLibs/DateFormatInfoScriptlet.java
+++ trinidad-impl/src/main/java/org/apache/myfaces/trinidadinternal/renderkit/core/xhtml/jsLibs/DateFormatInfoScriptlet.java
@@ -23,6 +23,7 @@ import java.io.IOException;
 import javax.faces.context.FacesContext;
 import javax.faces.context.ResponseWriter;
 
+import org.apache.myfaces.trinidad.context.RequestContext;
 import org.apache.myfaces.trinidad.context.RenderingContext;
 import org.apache.myfaces.trinidad.util.IntegerUtils;
 
@@ -59,8 +60,7 @@ class DateFormatInfoScriptlet extends Scriptlet
     ResponseWriter writer = context.getResponseWriter();
 
     writer.writeText("var _df2DYS=", null);
-    int twoDigitYearStart =
-      arc.getLocaleContext().getTwoDigitYearStart();
+    int twoDigitYearStart = RequestContext.getCurrentInstance().getTwoDigitYearStart();
     writer.writeText(IntegerUtils.getString(twoDigitYearStart), null);
     writer.writeText(";", null);
   }
