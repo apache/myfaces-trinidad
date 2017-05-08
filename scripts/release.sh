447fe8055cb751136f1303437aa7b999fde0fac1
Bernd Bohmann <bommel@apache.org>
2017-05-08 11:37:23 +0000

    prepare release

--- scripts/release.sh
+++ scripts/release.sh
@@ -16,35 +16,95 @@
 # limitations under the License.
 # -----------------------------------------------------------------------------
 
-# settings:
-VERSION=2.0.0-beta-3
-REPOSITORY=https://repository.apache.org/content/repositories/releases
+set -e
 
-# download the bits (user-agent to "" is important)
-wget --user-agent="" $REPOSITORY/org/apache/myfaces/trinidad/trinidad-assembly/$VERSION/trinidad-assembly-$VERSION-dist.tar.gz
-wget --user-agent="" $REPOSITORY/org/apache/myfaces/trinidad/trinidad-assembly/$VERSION/trinidad-assembly-$VERSION-dist.zip
-wget --user-agent="" $REPOSITORY/org/apache/myfaces/trinidad/trinidad-example-assembly/$VERSION/trinidad-example-assembly-$VERSION-example.tar.gz
-wget --user-agent="" $REPOSITORY/org/apache/myfaces/trinidad/trinidad-example-assembly/$VERSION/trinidad-example-assembly-$VERSION-example.zip
-wget --user-agent="" $REPOSITORY/org/apache/myfaces/trinidad/trinidad/$VERSION/trinidad-$VERSION-source-release.zip
+echo "Copying the download artifacts from the repository to the site."
 
-# rename the dist to the name which is used on the site
-mv trinidad-assembly-$VERSION-dist.zip trinidad-$VERSION-dist.zip
-mv trinidad-assembly-$VERSION-dist.tar.gz trinidad-$VERSION-dist.tar.gz
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <version>"
+    exit 1
+fi
 
-# rename the example-dist to the name which is used on the site
-mv trinidad-example-assembly-$VERSION-example.zip trinidad-$VERSION-example.zip
-mv trinidad-example-assembly-$VERSION-example.tar.gz trinidad-$VERSION-example.tar.gz
+VERSION=$1
 
-# rename the source to the name which is used on the site
-mv trinidad-$VERSION-source-release.zip trinidad-$VERSION-src-all.zip 
+MAVEN_REPOSITORY=https://repository.apache.org/content/repositories/releases
+DIST_REPOSITORY=https://dist.apache.org/repos/dist/release/myfaces
 
-# call the GPG script 
-/home/matzew/gpg-sign-all
+# download file and hashes/signatures
+download() {
+  DIR="$1"
+  FILE_ON_REPO="$2"
+  FILE="$3"
 
-# uploading....
-## For alpha/beta (all non 'final' releases), we have to use these URLS:
-# minotaur.apache.org:/www/myfaces.apache.org/dist/binaries
-# minotaur.apache.org:/www/myfaces.apache.org/dist/source
-scp *-dist.* minotaur.apache.org:/www/www.apache.org/dist/myfaces/binaries
-scp *-example.* minotaur.apache.org:/www/www.apache.org/dist/myfaces/binaries
-scp *-src-all.* minotaur.apache.org:/www/www.apache.org/dist/myfaces/source
+  curl --fail "${MAVEN_REPOSITORY}/${DIR}/${VERSION}/${FILE_ON_REPO}"      -o ${FILE}
+  curl --fail "${MAVEN_REPOSITORY}/${DIR}/${VERSION}/${FILE_ON_REPO}.asc"  -o ${FILE}.asc
+  curl --fail "${MAVEN_REPOSITORY}/${DIR}/${VERSION}/${FILE_ON_REPO}.md5"  -o ${FILE}.md5
+  curl --fail "${MAVEN_REPOSITORY}/${DIR}/${VERSION}/${FILE_ON_REPO}.sha1" -o ${FILE}.sha1
+}
+
+# this performs check of the hashes (if this fails, something might went wrong absolutely)
+check() {
+  FILE="$1"
+
+  echo "Checking file ${FILE}: "
+  if [ -f /usr/bin/md5sum ]; then
+     md5sum -b "${FILE}" | cut "-d " -f1 > "${FILE}.md5.temp"
+  else
+     md5 -q "${FILE}" > "${FILE}.md5.temp"
+  fi
+
+  if ! diff --ignore-all-space "${FILE}.md5" "${FILE}.md5.temp" ; then
+    echo "Error: MD5 check failed!"
+    exit -1
+  fi
+  echo "  MD5 hash okay"
+
+  shasum -a 1 "${FILE}" | cut "-d " -f1 > "${FILE}.sha1.temp"
+  if ! diff --ignore-all-space "${FILE}.sha1" "${FILE}.sha1.temp" ; then
+    echo "Error: SHA1 check failed!"
+    exit -1
+  fi
+  echo "  SHA1 hash okay"
+
+  if ! gpg --verify "${FILE}.asc" ; then
+    echo "Error: GPG check failed!"
+    exit -1
+  fi
+  echo "  GPG signature okay"
+
+  # todo: change, if maven creates the sha256
+  shasum -a 256 "${FILE}" > "${FILE}.sha256"
+  echo "  SHA-256 created"
+
+}
+
+# this uploads the files into the svn dist repo
+upload() {
+  PATTERN="$1"
+  FOLDER="$2"
+
+  for file in $(find . -type file -name "${PATTERN}" -exec basename \{\} \; ) ; do
+    # echo $file;
+    svn import -m "Uploading Trinidad release artifact ${VERSION} to dist server" $file ${DIST_REPOSITORY}/${FOLDER}/$file;
+  done
+}
+
+# commands
+
+download "org/apache/myfaces/trinidad/trinidad-assembly"         "trinidad-assembly-${VERSION}-dist.tar.gz"            "trinidad-assembly-${VERSION}-dist.tar.gz"
+download "org/apache/myfaces/trinidad/trinidad-assembly"         "trinidad-assembly-${VERSION}-dist.zip"               "trinidad-assembly-${VERSION}-dist.zip"
+download "org/apache/myfaces/trinidad/trinidad-example-assembly" "trinidad-example-assembly-${VERSION}-example.tar.gz" "trinidad-example-assembly-${VERSION}-example.tar.gz"
+download "org/apache/myfaces/trinidad/trinidad-example-assembly" "trinidad-example-assembly-${VERSION}-example.zip"    "trinidad-example-assembly-${VERSION}-example.zip"
+download "org/apache/myfaces/trinidad/trinidad"                  "trinidad-${VERSION}-source-release.zip"              "trinidad-${VERSION}-source-release.zip"
+
+check "trinidad-assembly-${VERSION}-dist.tar.gz"
+check "trinidad-assembly-${VERSION}-dist.zip"
+check "trinidad-example-assembly-${VERSION}-example.tar.gz"
+check "trinidad-example-assembly-${VERSION}-example.zip"
+check "trinidad-${VERSION}-source-release.zip"
+
+rm -f *.temp
+
+upload "trinidad-assembly-${VERSION}-dist.*"           "binaries"
+upload "trinidad-example-assembly-${VERSION}-example.*"        "binaries"
+upload "trinidad-${VERSION}-source-release.*" "source"
